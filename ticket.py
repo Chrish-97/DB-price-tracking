f10d9f3d589baab83a391b034defbcd3002c7bf1
Max Gruber <maxgruber19@gmail.com>
2025-03-28 23:48:06 +0100

    added basic gh action to run python script on nightly basis

--- ticket.py
+++ ticket.py
@@ -70,6 +70,7 @@ def init_driver():
     options.add_argument('--disable-dev-shm-usage')
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
+    driver.set_window_size(1920, 1080)
     return driver
 
 
