f965f3ad3ec4615fefec003b4c198eff7b36216f
Chrish-97 <christoph.hepfner97@gmail.com>
2025-03-28 23:01:21 +0100

    Update ticket.py

--- ticket.py
+++ ticket.py
@@ -152,18 +152,20 @@ def screenshot_and_extract_journey_info(driver, screenshot_path, target_time=Non
             return None
         print("search button vorhanden")
 
-        try:
-            WebDriverWait(driver, 10).until(EC.invisibility_of_element_located((By.CSS_SELECTOR, "dialog[open]")))
-            print("popup vorhanden")
-        except TimeoutException:
-            print("Kein Popup gefunden oder Timeout beim Warten auf Popup")
+        # Warte auf den Schließ-Button des Popups
+        close_button = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, "//dialog[@aria-hidden='false']//button[@aria-label='close']"))
+        )
+        close_button.click()  # Schließe das Popup
+        print("Popup geschlossen")
 
-        # Warte, bis der Button wirklich klickbar ist
+        # Warte auf den Button 'Günstige Tickets sichern'
         search_button = WebDriverWait(driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'Günstige Tickets sichern')]"))
         )
-        search_button.click()
-        sleep(5)
+        
+        search_button.click()  # Klicke den Button 'Günstige Tickets sichern'
+        time.sleep(5)
         print("search button angeklickt")
 
         driver.save_screenshot(screenshot_path)
