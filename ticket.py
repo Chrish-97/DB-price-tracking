1d298f5a628e607ba89f0f8244942c8ec736cb88
Chrish-97 <christoph.hepfner97@gmail.com>
2025-03-28 23:59:42 +0100

    Update ticket.py

--- ticket.py
+++ ticket.py
@@ -72,6 +72,7 @@ def init_driver():
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
     driver.set_window_size(1920, 1080)
+    driver.maximize_window()
     return driver
 
 
