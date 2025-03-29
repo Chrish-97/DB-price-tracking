d7887dd0d517087c94d94f6c6181366e7ab65987
Chrish-97 <christoph.hepfner97@gmail.com>
2025-03-29 02:37:06 +0100

    Update chart.py

--- chart.py
+++ chart.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('ticket_prices.csv', delimiter=',', index_col=0)
 df.index = pd.to_datetime(df.index)
-plt.figure(figsize=(16, 8))
+plt.figure(figsize=(20, 12))
 df.plot()
-plt.xticks(rotation=45, ha='right')  # Drehe die Beschriftungen um 45 Grad
-plt.locator_params(axis='x', nbins=10)  # Zeigt ca. 10 Ticks an
+plt.xticks(rotation=45, ha='right')  
+plt.locator_params(axis='x', nbins=15)  
 plt.tight_layout()
 plt.savefig('sampledata.png')
