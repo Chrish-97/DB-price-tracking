19cbc49c54d2feeaa82864f52430c4c7137e5c84
Chrish-97 <christoph.hepfner97@gmail.com>
2025-03-29 02:32:19 +0100

    Update chart.py

--- chart.py
+++ chart.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pandas.read_csv('ticket_prices.csv', delimiter=',', index_col=0)
+df = pd.read_csv('ticket_prices.csv', delimiter=',', index_col=0)
 df.index = pd.to_datetime(df.index)
 plt.figure(figsize=(12, 6))
 df.plot()
