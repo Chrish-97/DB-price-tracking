611ed35403d43d93b49739d6b2d9605f488ea73f
Chrish-97 <christoph.hepfner97@gmail.com>
2025-04-07 22:00:53 +0200

    Update chart.py

--- chart.py
+++ chart.py
@@ -3,9 +3,13 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('ticket_prices.csv', delimiter=',', index_col=0)
 df.index = pd.to_datetime(df.index)
+
+df = df.tail(300)
 plt.figure(figsize=(20, 12))
 df.plot()
-plt.xticks(rotation=45, ha='right')  
-plt.locator_params(axis='x', nbins=15)  
+plt.xticks(rotation=45, ha='right') 
+ax = plt.gca()
+ax.xaxis.set_major_locator(mdates.AutoDateLocator())
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
 plt.tight_layout()
 plt.savefig('sampledata.png')
